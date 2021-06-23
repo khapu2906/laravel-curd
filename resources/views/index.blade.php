667fcb6b26dc4e65d3e52ecb6b8d13fe47879a10
KHAPU <daikhanh9260@gmail.com>
2021-06-23 17:27:48 +0700

    update index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,20 @@
-<h1>
-    Hello World! <strong>This is TEST!!</strong>
-</h1>
+<!DOCTYPE html>
+<html>
+    <header>
+        <style>
+            body{
+                background-color:#e8be8b;
+            }
+            #title{
+                text-align: center;   
+            }
+            #title > span > h1 {
+                font-size: 50px;
+                margin-top: 20%;
+            }
+        </style>
+    </header>
+    <body>
+        <div id="title"><span><h1>Hello!! This is <strong>Khapu/Laravel Package</strong></h1></span><div>
+    </body>
+</html>
